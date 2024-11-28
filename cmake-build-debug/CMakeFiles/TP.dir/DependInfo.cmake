a3f2913389c25155c7d2b361cbc1b6275578afcf
rafae <rafaelbaraosilva@hotmail.com>
2024-11-28 12:26:40 +0000

    Teste do tiago banana

---
+++ cmake-build-debug/CMakeFiles/TP.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/rafae/OneDrive/Ambiente de Trabalho/POO/Trabalho Pratico/caravana.cpp" "CMakeFiles/TP.dir/caravana.cpp.obj" "gcc" "CMakeFiles/TP.dir/caravana.cpp.obj.d"
+  "C:/Users/rafae/OneDrive/Ambiente de Trabalho/POO/Trabalho Pratico/combate.cpp" "CMakeFiles/TP.dir/combate.cpp.obj" "gcc" "CMakeFiles/TP.dir/combate.cpp.obj.d"
+  "C:/Users/rafae/OneDrive/Ambiente de Trabalho/POO/Trabalho Pratico/grelha.cpp" "CMakeFiles/TP.dir/grelha.cpp.obj" "gcc" "CMakeFiles/TP.dir/grelha.cpp.obj.d"
+  "C:/Users/rafae/OneDrive/Ambiente de Trabalho/POO/Trabalho Pratico/item.cpp" "CMakeFiles/TP.dir/item.cpp.obj" "gcc" "CMakeFiles/TP.dir/item.cpp.obj.d"
+  "C:/Users/rafae/OneDrive/Ambiente de Trabalho/POO/Trabalho Pratico/jogador.cpp" "CMakeFiles/TP.dir/jogador.cpp.obj" "gcc" "CMakeFiles/TP.dir/jogador.cpp.obj.d"
+  "C:/Users/rafae/OneDrive/Ambiente de Trabalho/POO/Trabalho Pratico/main.cpp" "CMakeFiles/TP.dir/main.cpp.obj" "gcc" "CMakeFiles/TP.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
