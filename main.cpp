cf9f901b1b20c4514d7411a8841af727ea5cf9b6
tiago <tiago.lopes.guerreiro@gmail.com>
2024-12-18 15:28:07 +0000

    pega otario

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "jogador.h"
 #include "item.h"
 #include <iostream>
-#include <unistd.h>
+#include <cstring>
 
 using namespace std;
 
@@ -10,32 +10,46 @@ int main() {
     Grelha grelha;
     Jogador jogador;
     Item item;
+    string opcao;
 
     char instante;
     if (!grelha.lerFicheiro("mapa.txt", jogador, item)) {
         cout << "Erro ao carregar o mapa!" << endl;
         return 1;
     }
-    do{
-        grelha.mostrarGrelha();
 
-        cout << "Jogador tem " << jogador.getMoedas() << " moedas." << endl;
-        cout << "Instante entre item " << item.getInstanteEntreItem() << endl;
-        cout << "Duracao item " << item.getDuracao() << endl;
-        cout << "Max itens " << item.getMaxItens() << endl;
+    cout << "======================================" << endl;
+    cout << "Fase 1 Da Simulacao:" << endl;
+    cout << "Comandos: config <nomeFicheiro>, sair." << endl;
+    getline(cin, opcao);
 
-        item.Arca(jogador);
+    if (strcmp(opcao.c_str(), "config mapa.txt") == 0) {
 
-        cout << "Jogador tem " << jogador.getMoedas() << " moedas." << endl;
+        do {
+            grelha.mostrarGrelha();
 
-        // cout << item.ItemAparecer(grelha);
+            cout << "Jogador tem " << jogador.getMoedas() << " moedas." << endl;
+            cout << "Instante entre item " << item.getInstanteEntreItem() << endl;
+            cout << "Duracao item " << item.getDuracao() << endl;
+            cout << "Max itens " << item.getMaxItens() << endl;
 
-        cout << "Avancar instante? S/N - ";
-        cin >> instante;
-        jogador.setInstante(jogador.getInstante() + 1);
-    }while(instante == 'S');
+            item.Arca(jogador);
 
-    cout << "Percorreu - " << jogador.getInstante() << " Instantes.";
+            cout << "Jogador tem " << jogador.getMoedas() << " moedas." << endl;
+
+            // cout << item.ItemAparecer(grelha);
+
+            cout << "Avancar instante? S/N - " << endl;
+            cin >> instante;
+            jogador.setInstante(jogador.getInstante() + 1);
+        } while (instante == 'S' || instante == 's');
+    } else{
+        cout << "Decidiu Sair, Ate A Proxima" << endl;
+        return 0;
+    }
+
+    cout << "Fim De Jogo" << endl;
+    cout << "Percorreu: " << jogador.getInstante() << " Instantes.";
 
     return 0;
 }
